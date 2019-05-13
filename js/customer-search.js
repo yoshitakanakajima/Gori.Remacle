84bead241601854062bb052efd0bba5704eca18d
yoshitakanakajima <buyu.naka2@gmail.com>
2019-05-13 16:30:25 +0900

    update ajax

--- js/customer-search.js
+++ js/customer-search.js
@@ -3,17 +3,34 @@ $(function(){
         searching: false,
         lengthChange: false,
         info: false,
-        ajax: {
-            url: "",
-            type: "GET",
-            data: function (d) {
-                d.keyword = $('#keyword').val();
-            }
-        },
-        colums: [
+        // ajax: {
+        //     url: "http://localhost:63342/Gori.Remacle/data/cust01-1.json",
+        //     type: "GET",
+        //     data: function (d) {
+        //         d.keyword = $('#keyword').val();
+        //     }
+        // },
+        ajax: { url: "http://localhost:63342/Gori.Remacle/data/cust01-1.json", dataSrc: '' },
+        columns: [
             {data: "customer_key_list"},
-            {data: "customer_name_sei_list"},
+            {data: "customer_name_seimei_list"},
             {data: "customer_mobile_no_list"},
+            {data: "customer_name_kana_sei_list"},
+            {data: "customer_name_kana_mei_list"},
+            {data: "customer_name_sei_list"},
+            {data: "customer_name_mei_list"},
+            {data: "customer_address_list"},
+            {data: "customer_remarks_list"},
+            {data: "transmission_error_flg_list"},
+        ],
+        columnDefs: [
+            {targets: [3], visible: false},
+            {targets: [4], visible: false},
+            {targets: [5], visible: false},
+            {targets: [6], visible: false},
+            {targets: [7], visible: false},
+            {targets: [8], visible: false},
+            {targets: [9], visible: false},
         ],
         language: {
             url: "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json"
