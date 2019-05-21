4942d4f7d1939910ff8bc2a6f861350c14dcabf9
yoshitakanakajima <buyu.naka2@gmail.com>
2019-05-21 20:34:21 +0900

    ユーザー追加、変更を実装

--- js/user-list.js
+++ js/user-list.js
@@ -24,18 +24,17 @@ $(function(){
             {data: "user_name"},
             {data: "user_email"},
             {data: "permission_cd"},
+            {data: "permission_name"},
             {data: "sort_no"},
         ],
         columnDefs: [
             {targets: [0], visible: false},
             {targets: [1], visible: false},
-            {targets: [6], visible: false},
+            {targets: [5], visible: false},
+            {targets: [7], visible: false},
             {
-                targets: [7],
+                targets: [8],
                 render: function (data, type, row) {
-                    // console.log(data);
-                    // console.log(type);
-                    // console.log(row);
                     return data.user_no == '' ? '<button type="submit" class="btn btn-primary btn-xs user-add-button">追加</button>' : '<button type="submit" class="btn btn-primary btn-xs user-modify-button">変更</button>';
                 },
                 data: null,
@@ -58,16 +57,113 @@ $(function(){
 
             // コントロール制御
             $("#form #dialogTitle").text("新規登録");
-            $("#form #sendRegistButton").show();
-            $("#form #sendUpdateButton").hide();
-            $("#form #inputNo").prop("disabled", true);
+            $("#form #regist-button").show();
+            $("#form #update-button").hide();
 
             // フォーカス
             setTimeout(function(){
-                $("#inputNo").focus();
+                $("#user-id").focus();
             }, 500);
 
         }).modal("show");
+    });
+
+    $("#regist-button").on("click", function () {
+        $.ajax({
+            url: "http://localhost:63342/Gori.Remacle/data/acct01-3.json",
+            type: "POST",
+            data: {
+                "user_id": $("#user-id").val(),
+                "password": $("#password").val(),
+                "password_comfirm": $("#password-comfirm").val(),
+                "user_name": $("#user-name").val(),
+                "user_email": $("#user-email").val(),
+                "permission_cd": $("#permission-cd").val(),
+                "permission_name": $("#permission-name").val()
+            },
+            timeout: 10000,  // 単位はミリ秒
+            dataType: "json",
+            success: function (result, textStatus, xhr) {
+                var status = result["property"]["status"];
+                var message = result["property"]["message"]
+
+                if (status == "1") {
+                    // テーブル更新
+                    $('#user-list').DataTable().ajax.url("http://localhost:63342/Gori.Remacle/data/acct01-4.json").load();
+                    $('#user-list').DataTable().ajax.reload();
+                } else {
+                    displayHeaderMessage("0", message);
+                }
+
+                // フォームを閉じる
+                $("#form").modal("hide");
+            },
+            error: function() {
+                displayHeaderErrorMessage(textStatus, error)
+            }
+        });
+    });
+
+    $('#user-list tbody').on("click", "button.user-modify-button", function() {
+        if ($(this).find('.dataTables_empty').length == 0) {
+            var data = $('#user-list').dataTable().fnGetData("#user-list > tbody > tr");
+
+            // ダイアログ表示
+            $('#form').on('show.bs.modal', function (event) {
+                // 取得したデータのセット
+                $("#user-id").val(data.user_id);
+                $("#user-name").val(data.user_name);
+                $("#user-email").val(data.user_email);
+                $("#permission-cd").val(data.permission_cd);
+
+                // コントロール制御
+                $("#form #dialogTitle").text("更新");
+                $("#form #regist-button").hide();
+                $("#form #update-button").show();
+
+                // フォーカス
+                setTimeout(function(){
+                    $("#user-id").focus();
+                }, 500);
+
+            }).modal("show");
+        }
+    });
+
+    $("#update-button").on("click", function () {
+        $.ajax({
+            url: "http://localhost:63342/Gori.Remacle/data/acct01-5.json",
+            type: "POST",
+            data: {
+                "user_id": $("#user-id").val(),
+                "password": $("#password").val(),
+                "password_comfirm": $("#password-comfirm").val(),
+                "user_name": $("#user-name").val(),
+                "user_email": $("#user-email").val(),
+                "permission_cd": $("#permission-cd").val(),
+                "permission_name": $("#permission-name").val()
+            },
+            timeout: 10000,  // 単位はミリ秒
+            dataType: "json",
+            success: function (result, textStatus, xhr) {
+                var status = result["property"]["status"];
+                var message = result["property"]["message"]
+
+                if (status == "1") {
+                    // テーブル更新
+                    $('#user-list').DataTable().ajax.url("http://localhost:63342/Gori.Remacle/data/acct01-4.json").load();
+                    $('#user-list').DataTable().ajax.reload();
+                } else {
+                    displayHeaderMessage("0", message);
+                }
+
+                // フォームを閉じる
+                $("#form").modal("hide");
+            },
+            error: function() {
+                displayHeaderErrorMessage(textStatus, error)
+            }
+        });
     });
 
 })
